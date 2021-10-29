49b48800b76ac5c50a3bf3745962d51a32de1e26
alex <alex830216@gmail.com>
2021-10-29 14:56:31 +0800

    新增routes

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe "Courses", type: :request do
       course1 = Course.create(title: "foo", description: "bar")
       course2 = Course.create(title: "bar", description: "foo")
 
-      get :index
+      # request 跟 controller 用法類似，request 是後來出的，用在 rails 5 之後可以提升測試的速度
+      # 參考:https://ithelp.ithome.com.tw/articles/10263416?sc=iThelpR
+      get '/courses'
 
       expect(assigns[:courses]).to eq([course1, course2])
       expect(response).to render_template("index")
