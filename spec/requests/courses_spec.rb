f277d4f4531acade33ccac38e76a91fea010857f
alex <alex830216@gmail.com>
2021-11-01 13:06:59 +0800

    FactoryGirl

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "Courses", type: :request do
   describe "GET /index" do
     # pending "add some examples (or delete) #{__FILE__}"
     it "assigns @courses and render template" do
-      course1 = Course.create(title: "foo", description: "bar")
-      course2 = Course.create(title: "bar", description: "foo")
+      course1 = Course.create(:course)
+      course2 = Course.create(:course)
 
+
+      
       # request 跟 controller 用法類似，request 是後來出的，用在 rails 5 之後可以提升測試的速度
       # 參考:https://ithelp.ithome.com.tw/articles/10263416?sc=iThelpR
       get '/courses'
